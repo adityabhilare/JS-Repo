981b25e45737e7727de89f09a4fd1e60b03dfdcc
adityabhilare <147392304+adityabhilare@users.noreply.github.com>
2025-01-17 17:35:19 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) // we are using nodejs.not browser
+
+console.log(3+3);
+console.log("Hitesh")
+
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+// object 
